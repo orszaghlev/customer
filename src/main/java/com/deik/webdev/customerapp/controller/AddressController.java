f285c83ed5cca0b2efc589a65268602d6bbe8733
unknown <orszaghlev@gmail.com>
2020-11-27 14:37:49 +0100

    Created address controller and dto

---
+++ src/main/java/com/deik/webdev/customerapp/controller/AddressController.java
@@ -0,0 +1,59 @@
+package com.deik.webdev.customerapp.controller;
+
+import com.deik.webdev.customerapp.dto.AddressDto;
+import com.deik.webdev.customerapp.dto.AddressRecordRequestDto;
+import com.deik.webdev.customerapp.exception.UnknownCountryException;
+import com.deik.webdev.customerapp.model.Address;
+import com.deik.webdev.customerapp.service.AddressService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AddressController {
+
+    private final AddressService service;
+
+    @GetMapping("/address")
+    public Collection<AddressDto> listAddresses(){
+        return service.getAllAddress()
+                .stream()
+                .map(model -> AddressDto.builder()
+                        .address(model.getAddress())
+                        .address2(model.getAddress2())
+                        .district(model.getDistrict())
+                        .city(model.getCity())
+                        .country(model.getCountry())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+
+    @PostMapping("/address")
+    public void record(@RequestBody AddressRecordRequestDto requestDto) {
+        try {
+            service.recordAddress(new Address(
+                    requestDto.getAddress(),
+                    requestDto.getAddress2(),
+                    requestDto.getDistrict(),
+                    requestDto.getCity(),
+                    requestDto.getCountry(),
+                    requestDto.getPostalCode(),
+                    requestDto.getPhone()
+            ));
+        } catch (UnknownCountryException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/com/deik/webdev/customerapp/dto/AddressDto.java
@@ -0,0 +1,20 @@
+package com.deik.webdev.customerapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressDto {
+
+    private String address;
+    private String address2;
+    private String district;
+    private String city;
+    private String country;
+
+}
