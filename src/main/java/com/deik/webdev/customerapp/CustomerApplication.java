8a22b3a1dd6c2ae49e3462295c36c7abc537e2b3
unknown <orszaghlev@gmail.com>
2020-12-02 10:55:31 +0100

    Updated properties

--- src/main/java/com/deik/webdev/customerapp/CustomerApplication.java
+++ src/main/java/com/deik/webdev/customerapp/CustomerApplication.java
@@ -4,6 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Run -> Configuration -> Environment Variables
+ *  - DB_HOST localhost /127.0.0.1
+ *  - DB_PORT 3306
+ *  - DB_NAME sakila
+ *  - DB_USER root
+ *  - DB_PASS secret
+ *
+ *  Example configuration
+ * @see {project.basedir}/src/main/resources/sql/sakila.sh
+ */
+
 @Slf4j
 @SpringBootApplication
 public class CustomerApplication {
